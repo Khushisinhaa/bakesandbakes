dcc7422181444470a8431b9b61fd5b1602aedccd
Khushisinhaa <119104230+Khushisinhaa@users.noreply.github.com>
2025-03-26 02:46:24 +0530

    Create Best seller.js

---
+++ Best seller.js
@@ -0,0 +1,42 @@
+export const kurtaPage1=[
+    {
+        "image": "https://images.unsplash.com/photo-1605807646983-377bc5a76493?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fGJsYWNrJTIwZm9yZXN0JTIwY2FrZXxlbnwwfHwwfHx8MA%3D%3D",
+        "brand": "Rashmi Bakery",
+        "title": "1/2 kg Black Forest Cake",
+        "color": "",
+        "selling_price": "₹452",
+        "price": "₹800",
+        "disscount": "44% off",
+        "size": ""
+    },
+    {
+        "image": "https://plus.unsplash.com/premium_photo-1713920189815-876dbdf5f56e?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cmVkJTIwdmVsdmV0JTIwY2FrZXxlbnwwfHwwfHx8MA%3D%3D",
+        "brand": "Rashmi Bakery",
+        "title": "1 kg Red Velvet Cake",
+        "color": "",
+        "selling_price": "₹1,005",
+        "price": "₹1500",
+        "disscount": "33% off",
+        "size": ""
+    },
+    {
+        "image": "https://media.istockphoto.com/id/1035109138/photo/chocolate-cake-with-bonbon.webp?a=1&b=1&s=612x612&w=0&k=20&c=jhvMcFQtH7vinmgxst4aesPjDUCd-lUNJVDJioYDW0c=",
+        "brand": "Rashmi Bakery",
+        "title": "1 kg Chocolate truffle cake",
+        "color": "",
+        "selling_price": "₹1206",
+        "price": "₹1800",
+        "disscount": "33% off",
+        "size": ""
+    },
+    {
+        "image": "https://plus.unsplash.com/premium_photo-1663839331564-2a43583cea49?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8VmFuaWxsYSUyMENha2V8ZW58MHx8MHx8fDA%3D",
+        "brand": "Rashmi Bakery",
+        "title": "Vanilla Cake",
+        "color": "",
+        "selling_price": "₹452",
+        "price": "₹800",
+        "disscount": "44% off",
+        "size": ""
+    }
+    ]
